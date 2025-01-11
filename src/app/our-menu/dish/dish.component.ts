065c79dc516d37a44b09003f6c920b12cb221bf2
HPetersen2 <149525216+HPetersen2@users.noreply.github.com>
2025-01-11 17:28:20 +0100

    first commit

---
+++ src/app/our-menu/dish/dish.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dish',
+  standalone: true,
+  imports: [],
+  templateUrl: './dish.component.html',
+  styleUrls: ['./dish.component.scss',
+    '../our-menu.component.scss'
+  ]
+})
+export class DishComponent {
+
+}
